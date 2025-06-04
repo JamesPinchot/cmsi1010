078e3e4f37d902dcf607134453239ace15056cd7
JamesPinchot <127985895+JamesPinchot@users.noreply.github.com>
2025-06-03 23:02:51 -0700

    Add files via upload

---
+++ lab01/trianglelab.py
@@ -0,0 +1,11 @@
+def print_triangle(character, lines):
+    for count in range(1, lines + 1):
+        print(character * count)
+
+print_triangle(character="@", lines=8)
+print_triangle(character="&", lines=13)
+print_triangle(character="o", lines=5)
+
+for size in range(1, 21):
+    print_triangle(character="#", lines=size)
+    print()  #
